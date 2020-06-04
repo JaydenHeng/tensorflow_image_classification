4cc04ec993e017fa3d2f353353f94f74adfb6805
JaydenHeng <jayden_heng@hotmail.com>
2020-06-04 11:07:11 +0200

    code cleanup

--- training_script.py
+++ training_script.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-import datetime #for tensorboard usage
+import datetime  # for TensorBoard usage
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
-# from test_tflite import evaluate_tflite
 
 # SOL to activate swish activation
 from tensorflow.keras import backend as K
@@ -15,52 +13,53 @@ from tensorflow.keras.utils import get_custom_objects
 from tensorflow.keras.layers import Activation
 
 IMAGE_SIZE = 224
-BATCH_SIZE = 32
 IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
-train_generator = None
-val_generator = None
-model = None
+
 
 def swish(x):
     return K.sigmoid(x) * x
 
 
-def data_preparation():
-    global train_generator
-    global val_generator
+def get_callback():
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
+                                                          histogram_freq=1)
+    ##################################################################
+    ######################### Early Stopping #########################
+    ##################################################################
+    # Parameter: monitor = keep track of the quantity that is used to decide if the training should be terminated. Note: can monitor 'val_accuracy' as well
+    #            min_delta = the threshold that triggers the termination. In this case, we require the accuracy should at least improve 0.00001
+    #            patience = number of "no improvement epochs" to wait until training is stopped
+    ##################################################################
+    earlystopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy',
+                                                              min_delta=0.00001,
+                                                              patience=10)
+    return [tensorboard_callback, earlystopping_callback]
 
-    base_dir = os.path.join('datasets', 'wrist_band_photos')
 
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255, validation_split=0.2)
+def data_preparation(dataset_dir, batch_size=32):
+    datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255,
+                                                              validation_split=0.2)
 
-    train_generator = datagen.flow_from_directory(base_dir,
+    train_generator = datagen.flow_from_directory(dataset_dir,
                                                   target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                  batch_size=BATCH_SIZE,
+                                                  batch_size=batch_size,
                                                   subset='training')
 
-    val_generator = datagen.flow_from_directory(base_dir,
+    val_generator = datagen.flow_from_directory(dataset_dir,
                                                 target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                batch_size=BATCH_SIZE,
+                                                batch_size=batch_size,
                                                 subset='validation')
-
-    for image_batch, label_batch in train_generator:
-        break
-    image_batch.shape, label_batch.shape
-
-    print('class_indices: ', train_generator.class_indices)
 
     labels = '\n'.join(sorted(train_generator.class_indices.keys()))
 
-    print('labels: \n')
-    print(labels)
-
-    with open('conti_labels.txt', 'w') as f:
+    with open('labels.txt', 'w') as f:
         f.write(labels)
 
+    return train_generator, val_generator
 
-def start_training():
-    global model
 
+def start_training(train_generator, val_generator, epochs=10):
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(16,
                                kernel_size=(3, 3),  # (3, 3) means 3x3 kernal_size
@@ -70,13 +69,13 @@ def start_training():
         tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Conv2D(32,
-                               kernel_size=(3, 3),  # (3, 3) means 3x3 kernal_size
+                               kernel_size=(3, 3),  # (3, 3) means 3x3 kernel_size
                                padding='same',
                                activation=swish),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Conv2D(64,
-                               kernel_size=(3, 3),  # (3, 3) means 3x3 kernal_size
+                               kernel_size=(3, 3),  # (3, 3) means 3x3 kernel size
                                padding='same',
                                activation=swish),
         tf.keras.layers.Dropout(0.2),
@@ -91,60 +90,39 @@ def start_training():
                   metrics=[tf.keras.losses.BinaryCrossentropy(from_logits=True, name='binary_crossentropy'),
                            'accuracy'])
 
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
-                                                          histogram_freq=1)
-
-    ##################################################################
-    ######################### Early Stopping #########################
-    ##################################################################
-    # Parameter: monitor = keep track of the quantity that is used to decide if the training should be terminated. Note: can monitor 'val_accuracy' as well
-    #            min_delta = the threshold that triggers the termination. In this case, we require the accuracy should at least improve 0.00001
-    #            patience = number of "no improvement epochs" to wait until training is stopped
-    ##################################################################
-    earlystopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy',
-                                                              min_delta=0.00001,
-                                                              patience=10)
-
     model.summary()
 
     print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
 
-    epochs = 100
-
     model.fit(train_generator,
               epochs=epochs,
               validation_data=val_generator,
-              callbacks=[tensorboard_callback, earlystopping_callback])
+              callbacks=get_callback())
 
-    # model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(1e-5), metrics=['accuracy'])
-
-    # model.summary()
-
-    # print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
-    # print(train_generator)
-    # print(val_generator)
+    return model
 
 
-def save_model():
-    saved_model_dir = 'save/conti_fine_tuning'
-    tf.saved_model.save(model, saved_model_dir)
+def save_model(model, model_dir, model_name=None, convert_to_tflite=False):
+    tf.saved_model.save(model, model_dir)
 
-    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-    tflite_model = converter.convert()
+    if convert_to_tflite:
+        converter = tf.lite.TFLiteConverter.from_saved_model(model_dir)
+        tflite_model = converter.convert()
 
-    tflite_model_name = 'conti_model_1'
-
-    with open(tflite_model_name + '.tflite', 'wb') as f:
-        f.write(tflite_model)
+        with open(model_name + '.tflite', 'wb') as f:
+            f.write(tflite_model)
 
 
 def run():
     get_custom_objects().update({'swish': Activation(swish)})
-    data_preparation()
-    start_training()
-    save_model()
-    # evaluate_tflite(tflite_model_name) #Function in test_tflite.py
+    train_generator, val_generator = data_preparation(dataset_dir=os.path.join('datasets', 'wrist_band_photos'))
+    model = start_training(train_generator=train_generator,
+                           val_generator=val_generator,
+                           epochs=100)
+    save_model(model=model,
+               model_dir='save/fine_tuning',
+               model_name='tflite_model_3',
+               convert_to_tflite=True)
 
 
 if __name__ == "__main__":
